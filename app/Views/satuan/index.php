779ae28c82df7dd1ac653dc5deb894cf0ccbf086
aripbudiman <muhamadarifbudiman22@gmail.com>
2022-07-07 21:29:14 +0700

    embuat tampilan satuan

--- app/Views/satuan/index.php
+++ app/Views/satuan/index.php
@@ -1,21 +1,59 @@
 <?= $this->extend('layouts/index'); ?>
 <?= $this->section('konten'); ?>
-<div class="row">
-    <div class="col col-lg-6 mx-4 mx-lg-2">
+<div class="row mx-2">
+    <div class="col-12 col-xl-4">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Collapsible Card Example</h3>
+                <h3 class="card-title">Tambah</h3>
                 <div class="card-tools">
-                    <!-- Collapse Button -->
                     <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                 </div>
-                <!-- /.card-tools -->
             </div>
-            <!-- /.card-header -->
             <div class="card-body">
-                The body of the card
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="nama_satuan">Nama Satuan</label>
+                            <input type="text" class="form-control" id="nama_satuan">
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-primary" id="tambah-satuan">Tambah</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <!-- /.card-body -->
+        </div>
+    </div>
+    <div class="col-12 col-xl-8">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title"><?= $title; ?></h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Nama Satuan</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td><button class="btn btn-danger">Delete</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
